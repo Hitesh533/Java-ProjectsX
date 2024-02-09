2d691017b19cd2796771ff98704e6f4081f1969d
Hitesh <Hiteshindia533@gmail.com>
2024-02-10 04:33:28 +0530

    sixth_commit

---
+++ loteryapp/src/main/java/com/companyx/loteryapp/LoteryappApplication.java
@@ -0,0 +1,13 @@
+package com.companyx.loteryapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoteryappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoteryappApplication.class, args);
+	}
+
+}
---
+++ loteryapp/src/test/java/com/companyx/loteryapp/LoteryappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.companyx.loteryapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoteryappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
